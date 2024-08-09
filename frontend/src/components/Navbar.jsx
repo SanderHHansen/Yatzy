ebeb85e1e3f07c2f9cc4ae952be72ade6483889d
SanderHHansen <sanderhustadhansen@icloud.com>
2024-08-09 21:43:01 +0200

    Home made begun

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,10 @@ import "./Navbar.css"
 export default function Navbar() {
     return(
         <nav className="navbar">
-            <h1> Hei! Det funket!</h1>
+            <img className= "logo" src="https://cdn-icons-png.flaticon.com/512/1804/1804046.png"/>
+            <h1 className = "title"> Welcome to Yatzy!</h1>
+            <button>Create game</button>
+            <button>Join game</button>
         </nav>
     )
 }
