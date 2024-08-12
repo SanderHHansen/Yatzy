969dd89ca2e0d49743c03438986aca63fc1713c4
SanderHHansen <sanderhustadhansen@icloud.com>
2024-08-12 10:52:58 +0200

    Updated scoreboard

--- frontend/src/components/Scoreboard.jsx
+++ frontend/src/components/Scoreboard.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import { useState } from "react";
 
 function Scoreboard() {
+  // State for players
+  const [players, setPlayers] = useState();
+
+  const addPlayer = (playerToBeAdded) => {
+    setPlayers([...players, playerToBeAdded]);
+  };
+
   return (
     <div>
       <p>"Har importert tabellen"</p>
       <table>
         <thead>
           <tr>
-            <th> Poeng </th>
+            <th> Scores </th> {/*// TODO Has to be deleted later. */}
           </tr>
         </thead>
         <tbody>
@@ -20,6 +27,51 @@ function Scoreboard() {
           </tr>
           <tr>
             <td> Threes </td>
+          </tr>
+          <tr>
+            <td> Fours </td>
+          </tr>
+          <tr>
+            <td> Fives </td>
+          </tr>
+          <tr>
+            <td> Sixes </td>
+          </tr>
+          <tr>
+            <td> Bonus </td>
+          </tr>
+          <tr>
+            <td> Sum (Upper): </td>
+          </tr>
+          <tr>
+            <td> One pair </td>
+          </tr>
+          <tr>
+            <td> Two pairs </td>
+          </tr>
+          <tr>
+            <td> Three of a kind </td>
+          </tr>
+          <tr>
+            <td> Four of a kind </td>
+          </tr>
+          <tr>
+            <td> Small straight </td>
+          </tr>
+          <tr>
+            <td> Large straight </td>
+          </tr>
+          <tr>
+            <td> Full house </td>
+          </tr>
+          <tr>
+            <td> Chance </td>
+          </tr>
+          <tr>
+            <td> Yatzy </td>
+          </tr>
+          <tr>
+            <td> Sum: </td>
           </tr>
         </tbody>
       </table>
