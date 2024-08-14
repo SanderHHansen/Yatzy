f4557f8e4a340aa0968abbaa827663f8fe4da804
SanderHHansen <sanderhustadhansen@icloud.com>
2024-08-14 12:51:40 +0200

    More CSS fixes

--- frontend/src/components/PlayArea.jsx
+++ frontend/src/components/PlayArea.jsx
@@ -1,9 +1,16 @@
+import DiceSection from "./DiceSection.jsx";
+
 function PlayArea() {
   return (
     <>
       <div className="boardContainer">
         <div className="board"></div>
-        <div className="boardBottom"></div>
+        <div className="boardBottom">
+          <p className="keepText"> Dice to keep </p>
+          <div className="dices">
+            <DiceSection />
+          </div>
+        </div>
       </div>
     </>
   );
