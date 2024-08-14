424c08cf5d43ce8cc7a8d5b88c776646600e8a3f
SanderHHansen <sanderhustadhansen@icloud.com>
2024-08-14 15:33:51 +0200

    CSS

--- frontend/src/components/PlayArea.jsx
+++ frontend/src/components/PlayArea.jsx
@@ -3,14 +3,17 @@ import DiceSection from "./DiceSection.jsx";
 function PlayArea() {
   return (
     <>
-      <div className="boardContainer">
-        <div className="board"></div>
-        <div className="boardBottom">
-          <p className="keepText"> Dice to keep </p>
-          <div>
-            <DiceSection />
+      <div className="rightContainer">
+        <div className="boardContainer">
+          <div className="board"></div>
+          <div className="boardBottom">
+            <p className="keepText"> Dice to keep </p>
+            <div>
+              <DiceSection />
+            </div>
           </div>
         </div>
+        <button className="rollDiceButton"> Roll Dice </button>
       </div>
     </>
   );
